d7798791435df1f15c80c6a28ff77bb6b3cc1c4b
jakubkopta <jakubkopta@gmail.com>
2024-02-11 23:26:38 +0100

    Interests

---
+++ src/components/interests/Interests.tsx
@@ -0,0 +1,29 @@
+
+const Interests = () => {
+
+    return (
+        <div className="h-screen flex flex-col items-center justify-center text-center">
+            <h1 className="text-5xl text-gray-800 text-center font-bold p-10">Interests</h1>
+            <div id="Interests" className="h-screen flex justify-center items-center ml-28 mr-28">
+                <p className="shadow-lg p-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.</p>
+                <img className="opacity-50 rounded-full w-1/2 h-1/2" src="src/images/3.png" alt=""/>
+                <p className="shadow-lg p-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.</p>
+            </div>
+            <p className="shadow-lg p-5 w-1/3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+        </div>
+    )
+}
+
+export default Interests;
