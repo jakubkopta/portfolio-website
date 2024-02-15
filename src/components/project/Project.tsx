8e70c3d07438c060b154c728d3b24ca101e60128
jakubkopta <jakubkopta@gmail.com>
2024-02-15 22:31:51 +0100

    Links to projects

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -5,15 +5,16 @@ interface Props {
     imagePath: string;
     language: string;
     icon: ReactNode;
+    githubName: string;
 }
 
-const Project = ({name, imagePath, language, icon}: Props) => {
+const Project = ({name, imagePath, language, icon, githubName}: Props) => {
     return (
         <div className="rounded-lg overflow-hidden shadow-lg relative cursor-pointer group hover:bg-gradient-to-b from-gray-300 to-gray-50">
             <img className="w-full h-52 object-cover bg-bottom group-hover:opacity-10" src={imagePath} alt={`${name} gif`}/>
             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-center">
                 <h1 className="text-3xl font-bold pb-5">{name}</h1>
-                <a className="flex justify-center items-center">
+                <a href={`https://github.com/jakubkopta/${githubName}`} className="flex justify-center items-center">
                     <p className="p-3 bg-gray-800 text-white cursor-pointer rounded-lg w-32">More info</p>
                 </a>
             </div>
--- src/components/project/Projects.tsx
+++ src/components/project/Projects.tsx
@@ -8,31 +8,43 @@ const Projects = () => {
             name: "Calculator",
             imagePath: "./images/calculator.gif",
             language: "JavaFX",
-            icon: <FaJava className="icon"/>
+            icon: <FaJava className="icon"/>,
+            githubName: "CalculatorJavaFX"
         },
         {
             name: "Snake",
             imagePath: "./images/snake.gif",
             language: "JavaFX",
-            icon: <FaJava className="icon"/>
+            icon: <FaJava className="icon"/>,
+            githubName: "Snake"
         },
         {
             name: "Order completing",
             imagePath: "./images/orders.gif",
             language: "Java",
-            icon: <FaJava className="icon"/>
+            icon: <FaJava className="icon"/>,
+            githubName: "InStoreFulfillment"
         },
         {
             name: "To Do List",
             imagePath: "./images/todo.gif",
             language: "React",
-            icon: <FaReact className="icon"/>
+            icon: <FaReact className="icon"/>,
+            githubName: "toDo"
         },
         {
             name: "Fleet Manager",
             imagePath: "./images/fm.gif",
             language: "Java",
-            icon: <FaJava className="icon"/>
+            icon: <FaJava className="icon"/>,
+            githubName: "fleet-manager-springboot"
+        },
+        {
+            name: "Valentines",
+            imagePath: "./images/valentines.gif",
+            language: "React",
+            icon: <FaReact className="icon"/>,
+            githubName: "valentines"
         }
     ]
 
@@ -41,16 +53,16 @@ const Projects = () => {
             <h1 className="text-5xl text-gray-800 text-center font-bold p-10">
                 Projects
             </h1>
-            <p className="text-gray-800 text-center m-28">
+            <p className="text-gray-800 text-center m-10 md:m-28">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
                 deserunt mollit anim id est laborum.
             </p>
-            <div className="ml-28 mr-28 grid lg:grid-cols-3 gap-12">
+            <div className="ml-10 mr-10 md:ml-28 md:mr-28 grid lg:grid-cols-3 gap-12">
                 {data.map((item, idx) => (
-                    <Project key={idx} name={item.name} imagePath={item.imagePath} language={item.language} icon={item.icon}/>
+                    <Project key={idx} name={item.name} imagePath={item.imagePath} language={item.language} icon={item.icon} githubName={item.githubName}/>
                 ))}
             </div>
         </div>
