61df2b5ec04f52c316c84e728e786e97faed0420
jakubkopta <jakubkopta@gmail.com>
2024-02-15 15:50:44 +0100

    Contact & Experience update

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,50 +7,55 @@ const Contact = () => {
     return (
 
         <div id="Contact" className="border-t-2 border-gray-800">
-            <h1 className="text-5xl text-gray-800 text-center font-bold p-5 bg-gray-200">
-                Contact
-            </h1>
+            <div className="flex justify-center items-center text-xl text-gray-600 font-bold p-5 bg-gray-200">
+                <a href={"#Home"} className="pr-5">Home</a>
+                <a href={"#Experience"} className="pr-5">Experience</a>
+                <a href={"#Projects"} className="pr-5">Projects</a>
+                <a href={"#Interests"} className="pr-5">Interests</a>
+            </div>
             <div className="flex flex-col lg:flex-row justify-around items-center gap-10 relative p-20">
                 <div className="bg-gray-600 absolute inset-0 z-[-1]"></div>
-                <div className="flex flex-col justify-between">
-                    <p className="text-5xl text-white font-bold p-4 italic">
+                <div className="flex flex-col items-center justify-between">
+                    <p className="text-5xl text-gray-200 font-bold mb-10 italic">
                         Get in Touch
                     </p>
-                    <ol className="flex gap-5 relative">
-                        <a href="https://www.facebook.com/jakub.kopta">
-                            <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
-                                <div
-                                    className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
-                                </div>
-                                <FaFacebookF size={30}/>
-                            </li>
-                        </a>
-                        <a href="https://www.instagram.com/jakubkopta?igsh=MWhnN3BtbnJkNHBpeA%3D%3D&utm_source=qr">
-                            <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
-                                <div
-                                    className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
-                                </div>
-                                <FaInstagram size={30}/>
-                            </li>
-                        </a>
-                        <a href="https://www.linkedin.com/in/jakub-kopta-01109026b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
-                            <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
-                                <div
-                                    className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
-                                </div>
-                                <FaLinkedin size={30}/>
-                            </li>
-                        </a>
-                        <a href="https://github.com/jakubkopta">
-                            <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
-                                <a href="https://github.com/jakubkopta"></a>
-                                <div
-                                    className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
-                                </div>
-                                <FaGithub size={30}/>
-                            </li>
-                        </a>
-                    </ol>
+                    <div>
+                        <ol className="flex gap-5 relative">
+                            <a href="https://www.facebook.com/jakub.kopta">
+                                <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
+                                    <div
+                                        className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
+                                    </div>
+                                    <FaFacebookF size={30}/>
+                                </li>
+                            </a>
+                            <a href="https://www.instagram.com/jakubkopta?igsh=MWhnN3BtbnJkNHBpeA%3D%3D&utm_source=qr">
+                                <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
+                                    <div
+                                        className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
+                                    </div>
+                                    <FaInstagram size={30}/>
+                                </li>
+                            </a>
+                            <a href="https://www.linkedin.com/in/jakub-kopta-01109026b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
+                                <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
+                                    <div
+                                        className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
+                                    </div>
+                                    <FaLinkedin size={30}/>
+                                </li>
+                            </a>
+                            <a href="https://github.com/jakubkopta">
+                                <li className="relative flex justify-center items-center text-white p-4 bg-gray-800 rounded-full cursor-pointer group hover:text-blue-200">
+                                    <a href="https://github.com/jakubkopta"></a>
+                                    <div
+                                        className="absolute top-0 left-0 bg-blue-200 h-full w-full rounded-full z-[-1] transition scale-90 group-hover:scale-125 group-hover:shadow-blue-200 group-hover:blur-sm">
+                                    </div>
+                                    <FaGithub size={30}/>
+                                </li>
+                            </a>
+                        </ol>
+                    </div>
                 </div>
                 <div className="flex flex-row-reverse gap-10">
                     <div className="bg-gray-800 h-52 w-52 rounded-tl-3xl rounded-br-3xl relative shadow-xl">
--- src/components/experience/Courses.tsx
+++ src/components/experience/Courses.tsx
@@ -15,7 +15,7 @@ const Courses = () => {
         },{
             company: "Coders Lab",
             title: "Scrum Lab",
-            duration: "16.07.2022 - 19.02.2023",
+            duration: "06.11.2022 - 18.11.2022",
             details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
                 "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
                 "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
@@ -24,7 +24,7 @@ const Courses = () => {
         },{
             company: "Coders Lab",
             title: "Basics of HTML & CSS",
-            duration: "16.07.2022 - 19.02.2023",
+            duration: "04.2022",
             details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
                 "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
                 "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
@@ -33,7 +33,7 @@ const Courses = () => {
         },{
             company: "Coders Lab",
             title: "FullStack Developer",
-            duration: "16.07.2022 - 19.02.2023",
+            duration: "19.02.2023 - 19.04.2023",
             details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
                 "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
                 "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
