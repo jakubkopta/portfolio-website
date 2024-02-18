e7f74f117489b962d36c4b31d588c54e9b586888
jakubkopta <jakubkopta@gmail.com>
2024-02-18 20:30:14 +0100

    Experience text update

--- src/components/experience/ExperienceData.tsx
+++ src/components/experience/ExperienceData.tsx
@@ -5,40 +5,33 @@ const coursesData = [
         company: "Coders Lab",
         title: "Java Developer",
         duration: "07.2022 - 02.2023",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
-            "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
-            "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
-            "                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
-            "                deserunt mollit anim id est laborum."
+        details: "During this course I gained solid foundations and well-grounded knowledge of programming. " +
+            "In addition to Java, I have become familiar with tools and frameworks such as IntelliJ IDE, MySQL, Git, Spring and Hibernate. " +
+            "I also used knowledge that I gained to create several applications. One aspect I particularly value from this course is a habit for writing clean code."
     },{
         company: "Coders Lab",
         title: "Scrum Lab",
         duration: "11.2022 - 12.2022",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
-            "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
-            "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
-            "                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
-            "                deserunt mollit anim id est laborum."
+        details: "During this course I participated in a remote project simulating the everyday work of a programmer. " +
+            "I learned to work in a programming team using the SCRUM methodology. " +
+            "I enjoyed the collaborative aspect of our work, using tools like GitHub for version control, Trello for project management, and Slack for communication. " +
+            "Participating in code reviews and using gitflow improved my teamwork skills."
     },
     {
         company: "Coders Lab",
         title: "FullStack Developer",
         duration: "04.2023 - 07.2023",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
-            "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
-            "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
-            "                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
-            "                deserunt mollit anim id est laborum."
+        details: "Completing a full-stack course equipped me with essential skills in modern web development. " +
+            "I gained skills in front-end technologies like React and JavaScript, but also improved my HTML and CSS. " +
+            "Thanks to this knowledge I could create web applications like this website and dive more into front-end."
     },
     {
         company: "Coders Lab",
         title: "Basics of HTML & CSS",
         duration: "04.2022 - 05.2022",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
-            "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
-            "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
-            "                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
-            "                deserunt mollit anim id est laborum."
+        details: "Enrolling in an Basics of HTML & CSS course marked my entry into web development. " +
+            "Building my first website, I gained skills in HTML and CSS, laying the fundaments for further learning. " +
+            "Overall, the course provided me with a solid starting point to continue learning web development skills."
     }
 ];
 
@@ -47,20 +40,17 @@ const educationData = [
         company: "AGH University of Science and Technology",
         title: "Automatics and Robotics",
         duration: "10.2020 - current",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
-            "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
-            "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
-            "                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
-            "                deserunt mollit anim id est laborum."
+        details: "In university, studying Automatics and Robotics, I discovered my passion for software development. " +
+            "I realized my true interest lay in programming. This realization inspired me to pursue a path towards becoming a programmer. " +
+            "During this time I began teaching myself programming and taking courses alongside my studies. " +
+            "That leaded me to knowledge that I have today."
     },{
         company: "4TH General High School",
         title: "Mathematics, Physics, Computer Science, English",
         duration: "09.2016 - 06.2019",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
-            "                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n" +
-            "                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n" +
-            "                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
-            "                deserunt mollit anim id est laborum."
+        details: "During my time in high school, my interest in programming began, initially I was fascinated with robotics. " +
+            "I started programming microcontrollers like Arduino to bring these robots to life, discovering the thrill of coding to control physical systems. " +
+            "This experience laid the groundwork for my choose of major."
     }
 ];
 
--- src/components/experience/ExperienceSite.tsx
+++ src/components/experience/ExperienceSite.tsx
@@ -1,5 +1,6 @@
 import WorkItem from "./ExperienceInfo.tsx";
 import { workData, coursesData, educationData } from "./ExperienceData.tsx";
+import {TfiFaceSad} from "react-icons/tfi";
 
 interface Props {
     toggleState: number;
@@ -20,7 +21,10 @@ const ExperienceSite = ({toggleState}: Props) => {
         <div id="Skills" className="w-full flex justify-center items-center">
             <div className="w-2/3">
                 {data === null ? (
-                    <p className="text-center font-bold">Nothing there but cannot wait to gain some!</p>
+                    <div className="flex flex-col justify-center items-center gap-10 p-10">
+                        <p className="font-bold text-xl">Nothing there but cannot wait to gain some!</p>
+                        <TfiFaceSad size={50}/>
+                    </div>
                 ) : (
                     data.map((item, idx) => (
                         <WorkItem key={idx} company={item.company} title={item.title} duration={item.duration}
