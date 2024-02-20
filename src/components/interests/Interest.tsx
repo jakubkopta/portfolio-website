c08af6e30c3dfff44bbc00ff1a42dee84357cc13
Jakub Kopta <jakubkopta@gmail.com>
2024-02-20 15:53:06 +0100

    text size changes (interest)

--- src/components/interests/Interest.tsx
+++ src/components/interests/Interest.tsx
@@ -15,13 +15,16 @@ const Interest = ({name, imagePath, description, position}: Props) => {
 
     return (
         <div
-            className="flex flex-col justify-center items-center mt-10 cursor-pointer relative group lg:w-1/3 lg:hover:w-2/3 ease-in duration-500"
+            className="flex flex-col justify-center items-center md:w-[50vw] mt-10 cursor-pointer relative group lg:w-1/3 lg:hover:w-2/3 ease-in duration-500"
             onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            onMouseLeave={() => {
+                setIsHovered(false)
+                setTextButton(false)
+            }}
         >
             <img className="h-[50vh] rounded-xl opacity-30 object-cover md:h-[70vh] lg:h-full group-hover:opacity-100 duration-300"
                  src={imagePath} alt="img"/>
-            <span className="font-bold text-6xl opacity-80 absolute transform top-1/2 -translate-y-1/2 lg:text-8xl lg:-rotate-90 group-hover:opacity-100 group-hover:text-5xl group-hover:rotate-0 group-hover:-top-10 ease-in duration-500">
+            <span className="font-bold text-[7vw] opacity-80 absolute transform top-1/2 -translate-y-1/2 lg:-rotate-90 group-hover:opacity-100 group-hover:text-5xl group-hover:rotate-0 group-hover:-top-10 ease-in duration-500">
                 {name}
             </span>
             <div className={`w-full h-full opacity-0 absolute ${position} md:w-auto md:h-auto lg:delay-500 group-hover:opacity-100`}>
