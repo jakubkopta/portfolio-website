ff630b9b3ee9a802a23c0837d202ab694de8c6b1
Jakub Kopta <jakubkopta@gmail.com>
2024-03-13 13:52:43 +0100

    Adding dynamic imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import React, { useState, Suspense } from "react";
 import NavBar from "./components/navbar/NavBar.tsx";
 import Home from "./components/Home.tsx";
 import './App.css'
-import Projects from "./components/project/Projects.tsx";
-import Interests from "./components/interests/Interests.tsx";
-import Contact from "./components/contact/Contact.tsx";
-import Experience from "./components/experience/Experience.tsx";
-import {useState} from "react";
-import Games from "./components/games/Games.tsx";
+
+const Projects = React.lazy(() => import("./components/project/Projects.tsx"));
+const Interests = React.lazy(() => import("./components/interests/Interests.tsx"));
+const Contact = React.lazy(() => import("./components/contact/Contact.tsx"));
+const Experience = React.lazy(() => import("./components/experience/Experience.tsx"));
+const Games = React.lazy(() => import("./components/games/Games.tsx"));
+
 
 function App() {
 
@@ -20,14 +22,18 @@ function App() {
         <>
             <div className={`${isDarkMode ? "bg-dark-mode text-slate-200" : "bg-slate-200 text-gray-800"}`}>
                 <Home toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode}/>
-                <Experience/>
-                <Projects/>
-                <Games isDarkMode={isDarkMode}/>
-                <Interests/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Experience/>
+                    <Projects/>
+                    <Games isDarkMode={isDarkMode}/>
+                    <Interests/>
+                </Suspense>
             </div>
             <div>
-                <Contact/>
-                <NavBar isDarkMode={isDarkMode}/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Contact/>
+                    <NavBar isDarkMode={isDarkMode}/>
+                </Suspense>
             </div>
         </>
     )
